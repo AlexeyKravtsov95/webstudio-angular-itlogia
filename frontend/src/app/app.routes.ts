8d1197da93baa3d227deaad07dc8ecca1d6b5b5c
AlexeyKravtsov95 <kravcov95@inbox.ru>
2026-06-13 20:51:52 +0700

    beginner blog

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Layout } from './shared/layout/layout';
 import { Main } from './views/main/main';
+import { Blog } from './views/blog/blog/blog';
 
 export const routes: Routes = [
   {
@@ -14,6 +15,10 @@ export const routes: Routes = [
       {
         path: '',
         loadChildren: () => import ('./views/user/auth.routes').then(m => m.authRoutes)
+      },
+      {
+        path: 'blog',
+        component: Blog,
       }
     ]
   }
